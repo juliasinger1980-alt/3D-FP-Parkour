f5520ac8bb110449c1d9fd629fd72660f2cb12a9
Smaybes <241846892+juliasinger1980-alt@users.noreply.github.com>
2026-06-02 00:46:10 +0200

    first

---
+++ scripts/player.gd
@@ -0,0 +1,166 @@
+extends CharacterBody3D
+
+@onready var camera: Camera3D = $head/Camera3D
+@onready var head: Node3D = $head
+
+var gravity_player := 40
+
+var move_input: Vector2
+var cur_speed := Vector3.ZERO
+var target_speed_gehen := 6.0
+var target_speed_sprint := 11.0
+var sliding_speed := 19.5
+var target_speed_grappling := 25.0
+var max_speed := 0.0
+var sprinting := false
+var dir := Vector3.ZERO
+var accel := 12
+
+var sprungstaerke := 15.0
+var air_control_mult := 0.2
+var sprungbuffertimer := 0.150
+var koyotebuffertimer := 0.150
+@onready var sprungbuffertimer_max = sprungbuffertimer
+@onready var koyotebuffertimer_max = koyotebuffertimer
+
+var cam_sway_rot_dir: Vector3
+var sway_lerp_str := 5
+var sway_amount_deg_sprint := 2
+var sway_amount_deg_walk := 1
+var orig_cam_rot: Vector3
+var x_rot := 0.0
+var y_rot := 0.0
+var mouse_sensi := 0.0015
+var fov_normal := 100.0
+var fov_sprinting := 110.0
+var cam_fov_lerp_val := 8
+@onready var original_cam_pos = camera.position
+
+var fliegend := false
+
+func _ready() -> void:
+	orig_cam_rot = camera.rotation
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		x_rot = -event.relative.y * mouse_sensi
+		camera.rotation.x += x_rot
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+		
+		y_rot = -event.relative.x * mouse_sensi
+		rotation.y += y_rot
+
+func _physics_process(delta: float) -> void:
+	if is_on_wall():
+		var n = get_wall_normal()
+		if cur_speed.dot(n) < 0:
+			cur_speed = cur_speed.slide(n)
+	
+	WASD_Movement(delta)
+	springen(delta)
+	sprint_state_set(delta)
+	max_speed_set(delta)
+	grav(delta)
+	cam_sway(delta)
+	cam_fov_set(delta)
+	cur_speed_set(delta)
+	vel_set(delta)
+	Debugging()
+	move_and_slide()
+	
+
+func WASD_Movement(delta):
+	dir = Vector3.ZERO
+	#BASIC X/Z-MOVEMENT
+	if Input.is_action_pressed("W"):
+		dir += -global_transform.basis.z
+	if Input.is_action_pressed("S"):
+		dir += global_transform.basis.z
+	if Input.is_action_pressed("A"):
+		dir += -global_transform.basis.x
+	if Input.is_action_pressed("D"):
+		dir += global_transform.basis.x
+	
+	move_input = Input.get_vector("A","D","W","S")
+	dir = dir.normalized()
+
+func sprint_state_set(delta):
+	if Input.is_action_pressed("Shift"):
+		sprinting = true
+	else:
+		sprinting = false
+
+func max_speed_set(delta):
+	if sprinting:
+		max_speed = target_speed_sprint
+	else:
+		max_speed = target_speed_gehen
+
+func cur_speed_set(delta):
+	if not is_on_floor():
+		cur_speed = lerp(cur_speed, max_speed * dir, accel * delta * air_control_mult)
+	else:
+		cur_speed = lerp(cur_speed, max_speed * dir, accel * delta)
+
+func vel_set(delta):
+	velocity.x = cur_speed.x
+	velocity.z = cur_speed.z
+
+func grav(delta):
+	if not fliegend:
+		if not is_on_floor():
+			velocity.y -= gravity_player * delta
+
+func springen(delta):
+	if Input.is_action_just_pressed("Space"):
+		sprungbuffertimer = 0.150
+	
+	if is_on_floor():
+		koyotebuffertimer = 0.150
+	
+	if not is_on_floor():
+		koyotebuffertimer -= delta
+	
+	koyotebuffertimer = clamp(koyotebuffertimer, 0, koyotebuffertimer_max)
+	
+	sprungbuffertimer -= delta
+	sprungbuffertimer = clamp(sprungbuffertimer, 0, koyotebuffertimer_max)
+	
+	if (is_on_floor() and sprungbuffertimer > 0) or (not is_on_floor() and koyotebuffertimer > 0 and Input.is_action_just_pressed("Space")):
+		velocity.y = sprungstaerke
+		koyotebuffertimer = 0
+
+func cam_sway(delta):
+	if move_input.x > 0:
+		if sprinting:
+			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z - deg_to_rad(sway_amount_deg_sprint), sway_lerp_str*delta)
+		else:
+			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z - deg_to_rad(sway_amount_deg_walk), sway_lerp_str*delta)
+			
+	elif move_input.x < 0:
+		if sprinting:
+			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z + deg_to_rad(sway_amount_deg_sprint), sway_lerp_str*delta)
+		else:
+			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z + deg_to_rad(sway_amount_deg_walk), sway_lerp_str*delta)
+	
+	else:
+		camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z, sway_lerp_str*delta)
+
+func cam_fov_set(delta):
+	if sprinting:
+		if move_input != Vector2.ZERO:
+			camera.fov = lerp(camera.fov, fov_sprinting, cam_fov_lerp_val*delta)
+	else:
+		camera.fov = lerp(camera.fov, fov_normal, cam_fov_lerp_val*delta)
+
+func Debugging():
+	#NO-GRAV modus (T)
+	if Input.is_action_just_pressed("T"):
+		if fliegend:
+			fliegend = false
+		elif not fliegend:
+			fliegend = true
+	if fliegend:
+		if Input.is_action_just_pressed("G"):
+			velocity.y += 50
