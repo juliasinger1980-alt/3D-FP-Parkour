ae1a08246fd3936a396b0a708fa442d005a35c83
Smaybes <241846892+juliasinger1980-alt@users.noreply.github.com>
2026-06-03 16:46:47 +0200

    wallrun 99%

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,13 +3,15 @@ extends CharacterBody3D
 @onready var camera: Camera3D = $head/Camera3D
 @onready var head: Node3D = $head
 
+@onready var wallcheck_l: RayCast3D = $WallcheckL
+@onready var wallcheck_r: RayCast3D = $WallcheckR
+
 @onready var fps_label: Label = $"../UI/Transform_repl/VBoxContainer/FPS"
 @onready var velocity_label: Label = $"../UI/Transform_repl/VBoxContainer/Velocity"
 @onready var sprint_toggle_label: Label = $"../UI/Transform_repl/VBoxContainer/Sprint_toggle"
 
-
 #GRAVITY
-var gravity_player := 40
+var gravity_player := 50
 
 #WASD_MOVEMENT
 var move_input: Vector2
@@ -22,7 +24,7 @@ var dir := Vector3.ZERO
 var accel := 12.0
 
 #JUMP
-var sprungstaerke := 15.0
+var sprungstaerke := 20.0
 var air_control_mult := 0.2
 var air_speed_incr := 4
 var sprungbuffertimer := 0.150
@@ -56,10 +58,27 @@ var slide_dampening := 0.999
 var slide_control_mult := 0.2
 var sliding_cam_offset := 0.6
 var sliding_cam_lerp_str := 8.0
-var ground_pound_speed := 15
+var ground_pound_speed := 20.0
 var slide_cooldown := 0.200
 @onready var sliding_cam_pos = original_cam_pos - Vector3(0, sliding_cam_offset, 0)
 @onready var slide_cooldown_max = slide_cooldown
+
+#WALLRUN
+var wallrunningL := false
+var wallrunningR := false
+var wallrun_boost := 4.0
+var target_speed_wallrun := 25.0
+var wall_dir: Vector3
+var wall_L_normal: Vector3
+var wall_R_normal: Vector3
+var sway_amount_deg_wallrunning := 4.0
+var wallrun_lerp_str := 3.0
+var gravity_wallrun := 28
+var min_wallrun_speed := 4
+var wallrun_jump_off_boost := 8
+var wallrun_jump_off_str := 20
+var just_wallran_L := false
+var just_wallran_R := false
 
 #DEBUGGING
 var fliegend := false
@@ -92,10 +111,12 @@ func _physics_process(delta: float) -> void:
 	cur_speed_set(delta)
 	vel_x_z_set(delta)
 	slide_ground_pound(delta)
+	wallrun(delta)
 	Debugging(delta)
 	Label_text(delta)
+	get_state()
 	move_and_slide()
-	print(walking_fov_cooldown)
+	#print(velocity.y)
 
 func WASD_Movement(delta):
 	dir = Vector3.ZERO
@@ -126,24 +147,30 @@ func max_speed_set(delta):
 		max_speed = target_speed_gehen
 
 func cur_speed_set(delta):
-	if sliding:
-		cur_speed *= slide_dampening
-		cur_speed = lerp(cur_speed, max_speed * dir, accel * delta * slide_control_mult)
-	elif not is_on_floor():
-		cur_speed = lerp(cur_speed, (max_speed + air_speed_incr) * dir, accel * delta * air_control_mult)
-	else:
-		cur_speed = lerp(cur_speed, max_speed * dir, accel * delta)
+	if not wallrunningL and not wallrunningR:
+		if sliding:
+			cur_speed *= slide_dampening
+			cur_speed = lerp(cur_speed, max_speed * dir, accel * delta * slide_control_mult)
+		elif not is_on_floor():
+			cur_speed = lerp(cur_speed, (max_speed + air_speed_incr) * dir, accel * delta * air_control_mult)
+		else:
+			cur_speed = lerp(cur_speed, max_speed * dir, accel * delta)
 
 func vel_x_z_set(delta):
-	velocity.x = cur_speed.x
-	velocity.z = cur_speed.z
+	if not wallrunningL and not wallrunningR:
+		velocity.x = cur_speed.x
+		velocity.z = cur_speed.z
 
 func grav(delta):
 	if not fliegend:
-		if not is_on_floor():
+		if wallrunningL or wallrunningR:
+			velocity.y -= gravity_wallrun * delta
+		elif not is_on_floor():
 			velocity.y -= gravity_player * delta
 
 func springen(delta):
+	if wallrunningL or wallrunningR:
+		return
 	if Input.is_action_just_pressed("Space"):
 		sprungbuffertimer = 0.150
 	
@@ -164,6 +191,9 @@ func springen(delta):
 		koyotebuffertimer = 0
 
 func slide_ground_pound(delta):
+	if wallrunningL or wallrunningR:
+		return
+	
 	if Input.is_action_just_pressed("Ctrl") and (
 		not is_on_floor()
 		or (slide_cooldown == 0 and move_input != Vector2.ZERO)):
@@ -172,6 +202,7 @@ func slide_ground_pound(delta):
 			velocity.y -= ground_pound_speed
 		cur_speed = sliding_speed * dir
 		sliding = true
+	
 	if sliding:
 		if Input.is_action_just_released("Ctrl") or cur_speed.length() <= min_slide_speed:
 			slide_cooldown = slide_cooldown_max
@@ -180,27 +211,100 @@ func slide_ground_pound(delta):
 			camera.position = lerp(camera.position, sliding_cam_pos, sliding_cam_lerp_str*delta)
 	else:
 		camera.position = lerp(camera.position, original_cam_pos, sliding_cam_lerp_str*delta)
-		
+	
 	slide_cooldown -= delta
 	slide_cooldown = clamp(slide_cooldown, 0, slide_cooldown_max)
 
-func cam_sway(delta):
-	if move_input.x > 0:
-		if sprinting:
-			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z - deg_to_rad(sway_amount_deg_sprint), sway_lerp_str*delta)
-		else:
-			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z - deg_to_rad(sway_amount_deg_walk), sway_lerp_str*delta)
+func wallrun(delta):
+	if (not just_wallran_L) and ((not wallrunningL and not wallrunningR) and (not is_on_floor() and wallcheck_l.is_colliding())):
+		wall_L_normal = wallcheck_l.get_collision_normal()
+		wall_dir = Vector3.UP.cross(wallcheck_l.get_collision_normal().normalized())
+		if velocity.dot(wall_dir) < 0:
+			wall_dir = -wall_dir
+		if cur_speed.length() > min_wallrun_speed:
+			cur_speed = cur_speed + wallrun_boost * wall_dir
+			if velocity.y < 2:
+				velocity.y = 6
+			wallrunningL = true
 			
-	elif move_input.x < 0:
-		if sprinting:
-			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z + deg_to_rad(sway_amount_deg_sprint), sway_lerp_str*delta)
-		else:
-			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z + deg_to_rad(sway_amount_deg_walk), sway_lerp_str*delta)
+	if (not just_wallran_R) and ((not wallrunningL and not wallrunningR) and (not is_on_floor() and wallcheck_r.is_colliding())):
+		wall_R_normal = wallcheck_r.get_collision_normal()
+		wall_dir = Vector3.UP.cross(wallcheck_r.get_collision_normal().normalized())
+		if velocity.dot(wall_dir) < 0:
+			wall_dir = -wall_dir
+		if cur_speed.length() > min_wallrun_speed:
+			cur_speed = cur_speed + wallrun_boost * wall_dir
+			if velocity.y < 2:
+				velocity.y = 6
+			wallrunningR = true
+		
+	#print("VEL: ", " X: ", int(velocity.x), " Z: ",  int(velocity.z))
+	#print("CUR_S", cur_speed)
+	#print(wallcheck_r.is_colliding())
 	
-	else:
-		camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z, sway_lerp_str*delta)
+	if wallrunningL:
+		if Input.is_action_just_pressed("Space"):
+			wallrunningL = false
+			velocity.x += (wallrun_jump_off_boost * dir).x
+			velocity.z += (wallrun_jump_off_boost * dir).z
+			velocity.y = wallrun_jump_off_str
+			just_wallran_L = true
+			just_wallran_R = false
+		if not wallcheck_l.is_colliding() or is_on_floor() or cur_speed.length() < min_wallrun_speed:
+			just_wallran_L = true
+			just_wallran_R = false
+			wallrunningL = false
+		if velocity.length() < target_speed_wallrun:
+			velocity.x = lerp(velocity.x , target_speed_wallrun * wall_dir.x, wallrun_lerp_str * delta)
+			velocity.z = lerp(velocity.z , target_speed_wallrun * wall_dir.z, wallrun_lerp_str * delta)
+		cur_speed.x = velocity.x
+		cur_speed.z = velocity.z
+		camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z - deg_to_rad(sway_amount_deg_wallrunning), sway_lerp_str*delta)
+		
+	if wallrunningR:
+		if Input.is_action_just_pressed("Space"):
+			wallrunningR = false
+			velocity.x += (wallrun_jump_off_boost * dir).x
+			velocity.z += (wallrun_jump_off_boost * dir).z
+			velocity.y = wallrun_jump_off_str
+			just_wallran_R = true
+			just_wallran_L = false
+		if not wallcheck_r.is_colliding() or is_on_floor() or cur_speed.length() < min_wallrun_speed:
+			just_wallran_R = true
+			just_wallran_L = false
+			wallrunningR = false
+		if velocity.length() < target_speed_wallrun:
+			velocity.x = lerp(velocity.x , target_speed_wallrun * wall_dir.x, wallrun_lerp_str * delta)
+			velocity.z = lerp(velocity.z , target_speed_wallrun * wall_dir.z, wallrun_lerp_str * delta)
+		cur_speed.x = velocity.x
+		cur_speed.z = velocity.z
+		camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z + deg_to_rad(sway_amount_deg_wallrunning), sway_lerp_str*delta)
+	
+	if is_on_floor():
+		just_wallran_L = false
+		just_wallran_R = false
+
+func cam_sway(delta):
+	if not wallrunningL and not wallrunningR:
+		if move_input.x > 0:
+			if sprinting:
+				camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z - deg_to_rad(sway_amount_deg_sprint), sway_lerp_str*delta)
+			else:
+				camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z - deg_to_rad(sway_amount_deg_walk), sway_lerp_str*delta)
+				
+		elif move_input.x < 0:
+			if sprinting:
+				camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z + deg_to_rad(sway_amount_deg_sprint), sway_lerp_str*delta)
+			else:
+				camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z + deg_to_rad(sway_amount_deg_walk), sway_lerp_str*delta)
+		
+		else:
+			camera.rotation.z = lerp(camera.rotation.z, orig_cam_rot.z, sway_lerp_str*delta)
 
 func cam_fov_set(delta):
+	if wallrunningL or wallrunningR:
+		camera.fov = lerp(camera.fov, fov_sliding, cam_fov_lerp_str*delta)
+		return
 	if sliding:
 		if move_input != Vector2.ZERO:
 			camera.fov = lerp(camera.fov, fov_sliding, cam_fov_lerp_str*delta)
@@ -220,7 +324,19 @@ func cam_fov_set(delta):
 func Label_text(delta):
 	fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
 	velocity_label.text = "VELOCITY: " + str(int(velocity.length()))
-	sprint_toggle_label.text = "SPRINTING" if sprinting else "WALKING"
+	sprint_toggle_label.text = get_state()
+
+func get_state() -> String:
+	if wallrunningL or wallrunningR:
+		return "WALLRUNNING"
+	elif sliding:
+		return "SLIDING"
+	elif sprinting:
+		return "SPRINTING"
+	elif move_input != Vector2.ZERO:
+		return "WALKING"
+	else:
+		return "IDLE"
 
 func Debugging(delta):
 	#NO-GRAV modus (T)
